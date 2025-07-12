7a4b48e5d362501782c51a50ffd3e6d77eff0605
Milly-code7 <millychenau7@gmail.com>
2025-07-12 16:41:29 +1000

    refactor: rename api folder to mockApi to align with mockData structure

--- src/api/pumps/url.ts
+++
@@ -1,8 +0,0 @@
-export const url = {
-    getPumpList: "/Pumps/GetPumpList",
-    getPumpDetail: "/Pumps/GetPumpDetail",
-    createPump: "/Pumps/CreatePump",
-    updatePump: "/Pumps/UpdatePump",
-    deletePump: "/Pumps/DeletePump",
-  };
-  
